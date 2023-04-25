238bed9bfe6dd51cb295b460d27ee87ff817c88d
CHARITAH <102732524+CHARITAH@users.noreply.github.com>
2023-04-25 13:13:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+//https://teachablemachine.withgoogle.com/models/rkwwAO0CE/
+
+Webcam.attach('#camera');
+
+function capture(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id = "captured_image" src = "'+data_uri+'"/>';
+    });
+}
+
+console.log('ml5 version:', ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/rkwwAO0CE/model.json',modelLoaded);
